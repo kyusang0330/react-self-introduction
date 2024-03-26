84d8101ffcb8ee93b2412b32f53f00f390edff98
kyusang0330 <cholongs123@naver.com>
2024-03-26 16:51:59 +0900

    img 추가

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -6,6 +6,14 @@ const Home = () => {
     return (
         <div className="Home">
             <h1>Home</h1>
+            <div className="img text-center">
+                <img src="/images/test.jpg" className="rounded" alt="test"/>
+            </div>
+            <div>
+                <p>
+                    안녕하세요. ------ 이규상입니다!
+                </p>
+            </div>
         </div>
     );
 };
