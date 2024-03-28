8d943e20d8e73c9fe888acfed84ddf342eb6870a
kyusang0330 <cholongs123@naver.com>
2024-03-28 23:09:38 +0900

    Footer.css 추가

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,21 @@
-import React from 'react'
-
+import React, {useState} from 'react'
+import '../CSS/Footer.css'
 
 const Footer = () => {
 
-    return(
-        <div className="footer text-center">
-            <p>Front-End-Developer : Lee Kyu Sang</p>
-            <p>Phone : 010-4578-1305</p>
-            <p>E-Mail : cholongs123@naver.com</p>
+    const [Kyu, setKyu] = useState('Kyu')
+
+    return (
+
+        <div className="footer">
+            <div className="footer-logo">
+                {Kyu}
+            </div>
+            <div className="footer-info">
+                <em>Front-End-Developer : Lee Kyu Sang</em>
+                <p>Phone : 010-4578-1305</p>
+                <em>E-Mail : cholongs123@naver.com</em>
+            </div>
         </div>
     );
 };
