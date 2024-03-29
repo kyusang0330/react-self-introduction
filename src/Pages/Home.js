fb7f35bebadb024318077d0774303247b10267f3
kyusang0330 <cholongs123@naver.com>
2024-03-29 17:23:57 +0900

    Home.css 추가

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,19 +1,20 @@
 import React from 'react';
-
+import '../CSS/Home.css'
 
 const Home = () => {
 
     return (
         <div className="Home">
             <h1>Home</h1>
-            <div className="img text-center">
-                <img src="/images/test.jpg" className="rounded" alt="test"/>
+            <div className="Home-info">
+                <img src="/images/test.jpg" className="img" alt="test"/>
+                <div className="Home-hi">
+                    <p>
+                        안녕하세요. ------ 이규상입니다!
+                    </p>
+                </div>
             </div>
-            <div>
-                <p>
-                    안녕하세요. ------ 이규상입니다!
-                </p>
-            </div>
+
         </div>
     );
 };
