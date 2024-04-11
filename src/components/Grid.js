fe4162179bc47381b440428452907afbd53a2993
kyusang0330 <cholongs123@naver.com>
2024-04-11 21:01:40 +0900

    Grid 수정

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -5,11 +5,12 @@ const Grid = (props) => {
 
     return (
         <div className="container">
-            <div className="row g-2">
+            <div className="row">
                 <div className="first col-6">
                     {props.grid1}
                 </div>
-                <div className="second col-6">
+                <div className="second col-6">     {/*여기 칸을 없애서 할수 있지 않을까
+                                                        여기 칸 없애고 한번 짜보기*/}
                     {props.grid2}
                 </div>
             </div>
