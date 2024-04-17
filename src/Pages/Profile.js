72c4475763ab05c4fb328952cccddd46abfd7c2d
kyusang0330 <cholongs123@naver.com>
2024-04-17 22:16:46 +0900

    index 수정

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Grid from "../components/Grid";
-
+import "../index.css";
 
 const Profile = () => {
 
     return (
         <div className="Profile">
-            <h1>Profile</h1>
+            <h1>Profile Keyword</h1>
             <Grid grid1="학생회장" grid2="교육조교"/>
             <Grid grid1="교육부장" grid2="사무부장"/>
         </div>
