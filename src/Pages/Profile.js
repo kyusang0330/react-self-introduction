0e958ecf3b5c8c7a81633b31e477f9d128fea0c0
kyusang0330 <cholongs123@naver.com>
2024-04-18 19:21:26 +0900

    프로필 내용 추가

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -2,13 +2,23 @@ import React from 'react';
 import Grid from "../components/Grid";
 import "../index.css";
 
-const Profile = () => {
+function Profile() {
 
+    const ProfileContent = [
+        {title: '2017 대전대학교 전자.정보통신공학과 입학'},
+        {title: '2019 육군병장만기전역'},
+        {title: '2020 대전대학교 전자.정보통신공학과 학생회 교육부장'},
+        {title: '2021 대전대학교 정보통신.전자공학과 학생회 학생회장'}
+    ];
+    {/*위에 왜 출력안되는지 다시한번 찾아보기*/}
     return (
         <div className="Profile">
             <h1>Profile Keyword</h1>
             <Grid grid1="학생회장" grid2="교육조교"/>
             <Grid grid1="교육부장" grid2="사무부장"/>
+            <div className="Main-Profile">
+                {ProfileContent.title}
+            </div>
         </div>
     );
 };
