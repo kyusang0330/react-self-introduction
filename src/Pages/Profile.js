e02a424e049fc3a4f3e2dfd145a47f069d5b8199
kyusang0330 <cholongs123@naver.com>
2024-04-23 17:20:03 +0900

    Profile 수정

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Grid from "../components/Grid";
-import "../index.css";
+import "../CSS/Profile.css";
 
 function Profile() {
 
@@ -11,21 +11,25 @@ function Profile() {
         {title: '2021 대전대학교 정보통신.전자공학과 학생회 학생회장'}
     ];
 
-
+    const renderProfile = ProfileContent.map(profile => {
+        return (
+            <div className="ProfileContent">
+                {profile.title}
+            </div>
+        )
+    });
 
     return (
         <div className="Profile">
             <h1>Profile Keyword</h1>
             <Grid grid1="학생회장" grid2="교육조교"/>
             <Grid grid1="교육부장" grid2="사무부장"/>
-            <div className="Main-Profile">
-                {ProfileContent[0].title}
-                {ProfileContent[1].title}
-                {ProfileContent[2].title}
-                {ProfileContent[3].title}
+            <div className="MainProfile">
+                <img src="/images/Main.jpg" className="mainImg" alt="증명사진"/>
+                <div>{renderProfile}</div>
             </div>
         </div>
     );
-};
+}
 
 export default Profile;
