f8c726b76175dd9102dcbbbee40375b00b2e0a40
kyusang0330 <cholongs123@naver.com>
2024-04-25 20:49:21 +0900

    test

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -10,7 +10,7 @@ const Grid = (props) => {
                     {props.grid1}
                 </div>
                 <div className="second col-6">     {/*여기 칸을 없애서 할수 있지 않을까
-                                                        여기 칸 없애고 한번 짜보기*/}
+                                                        여기 칸 없애고 한번 짜보기* (반복제거)*/}
                     {props.grid2}
                 </div>
             </div>
