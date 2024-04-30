7ca994059d3e134aff5f034f915ab29b314ca4f8
kyusang0330 <cholongs123@naver.com>
2024-04-30 12:55:31 +0900

    styled components 추가

--- src/Pages/Hobby.js
+++ src/Pages/Hobby.js
@@ -1,16 +1,53 @@
-import React from 'react';
+import React, { useState } from "react";
 import Grid from "../components/Grid";
-
+import styled from "styled-components";
+import { useRecoilState } from "recoil";
+import { textState } from "../store";
 
 const Hobby = () => {
+  const [test, setTest] = useRecoilState(textState);
+  const hobbyList = ["Fooball", "Health", "Coffee", "Play Music"];
 
+  const HobbyListComponent = () => {
     return (
-        <div className="Hobby ">
-            <h1>Hobby</h1>
-            <Grid grid1="Fooball" grid2="Health"/>
-            <Grid grid1="Coffee" grid2="Play Music"/>
-        </div>
+      <HobbyListContainer>
+        {hobbyList.map((hobby) => (
+          <HobbyListItem>{hobby}</HobbyListItem>
+        ))}
+      </HobbyListContainer>
     );
+  };
+
+  return (
+    <HobbyContainer>
+      <h2>{test}</h2>
+      <HobbyListComponent />
+    </HobbyContainer>
+  );
 };
 
 export default Hobby;
+
+const HobbyContainer = styled.div`
+  margin: 0 auto;
+  width: 60%;
+`;
+
+const HobbyListContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 15px;
+`;
+
+const HobbyListItem = styled.div`
+  box-sizing: border-box;
+  display: inline-block;
+  width: 250px;
+  height: 250px;
+  border-radius: 10px;
+  border: 1px solid blue;
+  text-align: center;
+  align-items: center;
+  line-height: 250px;
+  color: black;
+`;
