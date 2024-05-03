585ab001a3917f2386ebd1152d743a081bf901f2
kyusang0330 <cholongs123@naver.com>
2024-05-03 18:46:06 +0900

    useEffect 사용

---
+++ src/Layout/index.js
@@ -0,0 +1,52 @@
+import Navbar from "../components/Navbar";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Home from "../Pages/Home";
+import Profile from "../Pages/Profile";
+import Hobby from "../Pages/Hobby";
+import Project from "../Pages/Project";
+import Footer from "../components/Footer";
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+
+const Layout = () => {
+  const [title, setTitle] = useState("Home");
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    switch (pathname) {
+      case "/":
+        setTitle("Home");
+        break;
+      case "/profile":
+        setTitle("Profile");
+        break;
+      case "/hobby":
+        setTitle("Hobby");
+        break;
+      case "/project":
+        setTitle("Project");
+        break;
+      default:
+        setTitle("Home");
+    }
+  }, [pathname]);
+
+  return (
+    <LayoutContainer>
+      <Navbar />
+      <h1>{title}</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/hobby" element={<Hobby />} />
+        <Route path="/project" element={<Project />} />
+      </Routes>
+      <Footer />
+    </LayoutContainer>
+  );
+};
+export default Layout;
+
+const LayoutContainer = styled.div`
+  color: red;
+`;
