cbf5886bf263eb414118e930606590ee2bfe60ac
kyusang0330 <cholongs123@naver.com>
2024-05-08 17:26:30 +0900

    styled-component 추가

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,20 +1,38 @@
-import React from 'react';
-import '../CSS/Home.css'
+import React from "react";
+import styled from "styled-components";
 
 const Home = () => {
-
-    return (
-        <div className="Home">
-            <h1>Home</h1>
-            <div className="Home-info">
-                <img src="/images/Main.jpg" className="img" alt="증명사진"/>
-                <div className="Home-hi">
-                    <p>안녕하세요. ------ 이규상입니다!</p>
-                    <p>안녕하세요. ------ 이규상입니다!</p>
-                </div>
-            </div>
-        </div>
-    );
+  return (
+    <HomeContainer>
+      <HomeInfo>
+        <MainImg src="/images/Main.jpg" alt="증명사진" />
+        <HomeHi>
+          <HomeHiFirst>안녕하세요. ------ 이규상입니다!</HomeHiFirst>
+        </HomeHi>
+      </HomeInfo>
+    </HomeContainer>
+  );
 };
 
 export default Home;
+
+const HomeContainer = styled.div``;
+
+const HomeInfo = styled.div`
+  display: flex;
+  padding-bottom: 10px;
+`;
+
+const MainImg = styled.img`
+  width: 300px;
+  padding-left: 30px;
+  padding-right: 15px;
+`;
+
+const HomeHi = styled.div`
+  align-items: center;
+`;
+
+const HomeHiFirst = styled.p`
+  font-size: 20px;
+`;
