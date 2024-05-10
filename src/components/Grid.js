852f9fa51e330635e3be56d49a35f8315affd6c0
kyusang0330 <cholongs123@naver.com>
2024-05-10 16:43:47 +0900

    Grid

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,22 +1,15 @@
-import React from 'react';
-import "../CSS/Grid.css"
+import React from "react";
+import "../CSS/Grid.css";
 
 const Grid = (props) => {
-
-    return (
-        <div className="container">
-            <div className="row">
-                <div className="first col-6">
-                    {props.grid1}
-                </div>
-                <div className="second col-6">     {/*여기 칸을 없애서 할수 있지 않을까
-                                                        여기 칸 없애고 한번 짜보기* (반복제거)*/}
-                    {props.grid2}
-                </div>
-            </div>
-        </div>
-    );
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="first col-6">{props.grid1}</div>
+        <div className="second col-6"> {props.grid2}</div>
+      </div>
+    </div>
+  );
 };
-
 
 export default Grid;
