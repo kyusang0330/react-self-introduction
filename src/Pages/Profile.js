d07ab82032b2cf0949eba3d03ffe17c2cad094db
kyusang0330 <cholongs123@naver.com>
2024-05-14 19:10:57 +0900

    Profile

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -3,12 +3,12 @@ import styled from "styled-components";
 
 function Profile() {
   const ProfileContent = [
-    { title: "2017 대전대학교 전자.정보통신공학과 입학" },
-    { title: "2019 육군병장만기전역" },
-    { title: "2020 대전대학교 전자.정보통신공학과 학생회 교육부장" },
-    { title: "2021 대전대학교 정보통신.전자공학과 학생회 학생회장" },
-    { title: "2022 대전대학교 공과대학 사무부장" },
-    { title: "2023 대전대학교 정보통신공학과 교육조교" },
+    { title: "2017 대전대학교 전자.정보통신공학과 입학." },
+    { title: "2019 육군병장만기전역." },
+    { title: "2020 대전대학교 전자.정보통신공학과 학생회 교육부장." },
+    { title: "2021 대전대학교 정보통신.전자공학과 학생회 학생회장." },
+    { title: "2022 대전대학교 공과대학 사무부장." },
+    { title: "2023 대전대학교 정보통신공학과 교육조교." },
   ];
 
   const renderProfile = ProfileContent.map((profile) => {
