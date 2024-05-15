6efc2ad6949f79a68bc486e7d9805e23657e07a6
kyusang0330 <cholongs123@naver.com>
2024-05-15 22:28:48 +0900

    Hobby

--- src/Pages/Hobby.js
+++ src/Pages/Hobby.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
-import Grid from "../components/Grid";
-import styled from "styled-components";
+import {
+  HobbyListContainer,
+  HobbyListItem,
+  HobbyContainer,
+} from "../styles/hobby_styled";
 
 const Hobby = () => {
   const hobbyList = ["Football", "Health", "Coffee", "Play Music"];
@@ -23,27 +26,3 @@ const Hobby = () => {
 };
 
 export default Hobby;
-
-const HobbyContainer = styled.div`
-  margin: 0 auto;
-  width: 60%;
-`;
-
-const HobbyListContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  gap: 15px;
-`;
-
-const HobbyListItem = styled.div`
-  box-sizing: border-box;
-  display: inline-block;
-  width: 250px;
-  height: 250px;
-  border-radius: 10px;
-  border: 1px solid blue;
-  text-align: center;
-  align-items: center;
-  line-height: 250px;
-  color: black;
-`;
