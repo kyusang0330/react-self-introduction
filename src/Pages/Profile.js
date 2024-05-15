cbee7fe37f067becfe12c5b60f2f05c2def9bcbf
kyusang0330 <cholongs123@naver.com>
2024-05-15 22:28:34 +0900

    Profile

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -1,5 +1,11 @@
 import Grid from "../components/Grid";
-import styled from "styled-components";
+import {
+  ProfileContainer,
+  MainProfile,
+  ProfileMainImg,
+  MainProfileText,
+  ContentText,
+} from "../styles/profile_styled";
 
 function Profile() {
   const ProfileContent = [
@@ -30,23 +36,3 @@ function Profile() {
 }
 
 export default Profile;
-
-const ProfileContainer = styled.div`
-  text-align: center;
-`;
-
-const MainProfile = styled.div`
-  display: flex;
-  justify-content: center;
-`;
-
-const ProfileMainImg = styled.img`
-  width: 300px;
-  padding: 0 10px 10px 10px;
-`;
-
-const MainProfileText = styled.div``;
-
-const ContentText = styled.div`
-  text-align: start;
-`;
