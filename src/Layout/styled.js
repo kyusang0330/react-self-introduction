7b72d9d7ebcfa4d222fdcb45b6d55485364a2a76
kyusang0330 <cholongs123@naver.com>
2024-05-17 17:43:01 +0900

    style-component

--- src/Layout/styled.js
+++ src/Layout/styled.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 const LayoutContainer = styled.div`
   color: blue;
-  margin-bottom: 100px; //useEffect 사용해보기
+  margin-bottom: 120px; //useEffect 사용해보기
 `;
 
 export { LayoutContainer };
