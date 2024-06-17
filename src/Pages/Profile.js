167c944855cbce5a5bd8ddf0f913465b6b63bdf1
kyusang0330 <cholongs123@naver.com>
2024-06-17 16:36:10 +0900

    Profile

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -90,7 +90,6 @@ function Profile() {
       </MainProfile>
       <RenderProfile />
       <Grid grid1="학생회장" grid2="교육조교" />
-      <Grid grid1="교육부장" grid2="사무부장" />
     </ProfileContainer>
   );
 }
