3a7673d61d4ad3d8edced8adc05c82e19b986cda
kyusang0330 <cholongs123@naver.com>
2024-07-02 18:07:09 +0900

    Hobby

--- src/Pages/Hobby.js
+++ src/Pages/Hobby.js
@@ -12,7 +12,7 @@ import {
 const Hobby = () => {
   const hobbyList = [
     {
-      img: "../images/sports.jpg",
+      img: "../images/hobbyimg/sports.jpg",
       title: "Sports",
       desc:
         "고등학생 때부터 주말마다 풋살을 하였습니다. " +
@@ -21,7 +21,7 @@ const Hobby = () => {
       name: "스포츠",
     },
     {
-      img: "../images/health.jpg",
+      img: "../images/hobbyimg/health.jpg",
       title: "Health Training",
       desc:
         "고등학교 때 다이어트를 위해 헬스장에 처음 입문하여 방학 때마다 헬스장을 다녔습니다. " +
@@ -30,7 +30,7 @@ const Hobby = () => {
       name: "헬스",
     },
     {
-      img: "../images/coffee.jpg",
+      img: "../images/hobbyimg/coffee.jpg",
       title: "Coffee",
       desc:
         "20살 때, 처음 카페를 가보았고, 아아라는 줄임말이 유행하여 아이스 아메리카노를 처음 마셔보고 왜 마시나 싶었지만," +
@@ -38,7 +38,7 @@ const Hobby = () => {
       name: "카페",
     },
     {
-      img: "../images/music.jpg",
+      img: "../images/hobbyimg/music.jpg",
       title: "Play Music",
       desc:
         "대중교통을 이용하거나 차를 운전하고, 운동할 때 노래를 즐겨 듣습니다. " +
