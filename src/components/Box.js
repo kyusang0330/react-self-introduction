a0a0f5136210a248fda910698bfffce193fb3255
kyusang0330 <cholongs123@naver.com>
2024-07-08 22:10:40 +0900

    test

--- src/components/Box.js
+++ src/components/Box.js
@@ -3,14 +3,12 @@ import React, { useState, useEffect } from "react";
 import {
   BoxContainer,
   BoxMain,
-  TestBox,
   TestText,
   TestText2,
-  TestText3,
 } from "../styles/box_styled";
 
 const Box = () => {
-  const [showAnimation, setShowAnimation] = useState(true);
+  /* const [showAnimation, setShowAnimation] = useState(true);
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -19,28 +17,39 @@ const Box = () => {
 
     return () => clearTimeout(timer);
   });
+*/
+  const boxVariants = {
+    out: { y: 600 },
+    in: {
+      y: 0,
+      transition: {
+        duration: 0.5,
+        delayChildren: 1,
+        staggerChildren: 0.5,
+      },
+    },
+  };
+
+  const iconVariants = {
+    out: { x: -600, scale: 0 },
+    in: { x: 0, scale: 1 },
+  };
   return (
     <BoxContainer>
-      {showAnimation && (
-        <BoxMain>
-          <TestBox
-            initial={{ scale: 0 }}
-            animate={{ scale: 1, rotateZ: 360 }}
-            transditon={{
-              duration: 3,
-              type: "spring",
-              stiffness: 260,
-              damping: 20,
-            }}
-          >
-            <TestText>Frond end</TestText>
-            <TestText2>Developer</TestText2>
-            <TestText3>Lee Kyu Sang</TestText3>
-          </TestBox>
-        </BoxMain>
-      )}
+      <BoxMain variants={boxVariants} initial="out" animate="in">
+        <TestText variants={iconVariants}>Frond-End Developer</TestText>
+        <TestText2 variants={iconVariants}>Lee Kyu Sang</TestText2>
+      </BoxMain>
     </BoxContainer>
   );
 };
+
+//     {showAnimation && (
+//         <BoxMain variants={boxVariants} initial="out" animate="in">
+//           <TestText variants={iconVariants}>Frond end</TestText>
+//           <TestText2 variants={iconVariants}>Developer</TestText2>
+//           <TestText3 variants={iconVariants}>Lee Kyu Sang</TestText3>
+//         </BoxMain>
+//       )}
 
 export default Box;
