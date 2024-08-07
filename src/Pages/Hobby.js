eda224ab2658a2dcba4dc10c5d4990ebdf3d980d
kyusang0330 <cholongs123@naver.com>
2024-08-07 16:15:07 +0900

    Hobby

--- src/Pages/Hobby.js
+++ src/Pages/Hobby.js
@@ -1,4 +1,8 @@
 import React from "react";
+import sports from "../assets/images/sports.jpg";
+import health from "../assets/images/health.jpg";
+import coffee from "../assets/images/coffee.jpg";
+import music from "../assets/images/music.jpg";
 import {
   HobbyListContainer,
   HobbyListItem,
@@ -12,7 +16,7 @@ import {
 const Hobby = () => {
   const hobbyList = [
     {
-      img: "/../images/sports.jpg",
+      img: sports,
       title: "Sports",
       desc:
         "고등학생 때부터 주말마다 풋살을 하였습니다. " +
@@ -21,7 +25,7 @@ const Hobby = () => {
       name: "sports",
     },
     {
-      img: "./../images/health.jpg",
+      img: health,
       title: "Health Training",
       desc:
         "고등학교 때 다이어트를 위해 헬스장에 처음 입문하여 방학 때마다 헬스장을 다녔습니다. " +
@@ -30,7 +34,7 @@ const Hobby = () => {
       name: "muscle",
     },
     {
-      img: "../images/coffee.jpg",
+      img: coffee,
       title: "Coffee",
       desc:
         "20살 때, 처음 카페를 가보았고, 아아라는 줄임말이 유행하여 아이스 아메리카노를 처음 마셔보고 왜 마시나 싶었지만," +
@@ -38,7 +42,7 @@ const Hobby = () => {
       name: "coffee",
     },
     {
-      img: "../images/music.jpg",
+      img: music,
       title: "Play Music",
       desc:
         "대중교통을 이용하거나 차를 운전하고, 운동할 때 노래를 즐겨 듣습니다. " +
