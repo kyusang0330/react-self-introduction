043d1f899454ed5b0364ca49028d3ca2e4da0a8c
kyusang0330 <cholongs123@naver.com>
2024-08-08 14:59:42 +0900

    Profile

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -1,4 +1,3 @@
-import Grid from "../components/Grid";
 import { ProfileContainer } from "../styles/profile_styled";
 import BasicProfile from "../components/profilecomponent/BasicProfile";
 import ProfileListArea from "../components/profilecomponent/ProfileListArea";
@@ -8,8 +7,6 @@ function Profile() {
     <ProfileContainer>
       <BasicProfile />
       <ProfileListArea />
-      <Grid grid1="학생회장" grid2="교육조교" />
-      <Grid grid1="교육부장" grid2="사무부장" />
     </ProfileContainer>
   );
 }
